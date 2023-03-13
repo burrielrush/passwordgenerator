c2c54a222383daf7610be34455004894cba7b917
Rush Burriel <burrielrush@gmail.com>
2023-03-13 13:21:46 -0600

    Made color and font changes in css

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 // Get the generate button element
 const generateBtn = document.querySelector("#generate");
 
-// Define the generatePassword() function
+
 function generatePassword() {
-  // Define the character sets to be used
+  
   const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
   const uppercaseChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   const numericChars = "0123456789";
   const specialChars = "!@#$%^&*()_+";
 
-  // Prompt the user for which character sets to include
+ 
   const includeLowercase = confirm("Include lowercase letters?");
   const includeUppercase = confirm("Include uppercase letters?");
   const includeNumeric = confirm("Include numbers?");
   const includeSpecial = confirm("Include special characters?");
 
-  // Build the character set for the password
+
   let charset = "";
   if (includeLowercase) {
     charset += lowercaseChars;
@@ -30,7 +30,7 @@ function generatePassword() {
     charset += specialChars;
   }
 
-  // Generate the password
+
   let password = "";
   while (password.length < 8) {
     const randomIndex = Math.floor(Math.random() * charset.length);
